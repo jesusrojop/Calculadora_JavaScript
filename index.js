77fa86b169870042a86f497f3aa417f32c1e2ea6
ManuelsSaNt <95022226+ManuelsSaNt@users.noreply.github.com>
2022-10-18 09:47:36 -0600

    funcion del boton borrar uno

--- index.js
+++ index.js
@@ -9,125 +9,149 @@ window.addEventListener('load', ()=> {
 	// } )
 
 
-	//* obtenccion de comandos
+	//* Obtencion de comandos
 	const borrarTodo = document.querySelector("#borrarTodo")
+	const borrarUno = document.querySelector("#borrarUno")
+	const operadorIgual = document.querySelector("#operadorIgual")
+
+	//* Funcion de cada comando
+
+	// BORRAR TODO
 	borrarTodo.addEventListener('click', () => {
 		console.log("soy c");
 		document.querySelector('.valor-inferior').value = ''
 	})
 
-	const borrarUno = document.querySelector("#borrarUno")
+	// BORRAR UNO
 	borrarUno.addEventListener('click', ()=> {
 		console.log("soy borrar uno");
-		document.querySelector('.valor-inferior').value += ''
+		console.log(document.querySelector
+			('.valor-inferior').value)
 	})
 
-	const operadorIgual = document.querySelector("#operadorIgual")
+	// IGUAL
 	operadorIgual.addEventListener('click', () => {
 		console.log("soy igual")
 		document.querySelector('.valor-inferior').value = ''
 	})
 
-	// saisaijsajadsajs
-
-	
-	//* obtenccion de numeros
+	//* Obtencion de numeros
 
 	const numeroCero = document.querySelector("#cero")
+	const numeroUno = document.querySelector("#uno")
+	const numeroDos = document.querySelector("#dos")
+	const numeroTres = document.querySelector("#tres")
+	const numeroCuatro = document.querySelector("#cuatro")
+	const numeroCinco = document.querySelector("#cinco")
+	const numeroSeis = document.querySelector("#seis")
+	const numeroSiete = document.querySelector("#siete")
+	const numeroOcho = document.querySelector("#ocho")
+	const numeroNueve = document.querySelector("#nueve")
+	const caracterDecimal = document.querySelector("#punto")
+
+
+	//* Funcionalidad de cada numero
+
+	// NUMERO CERO
 	numeroCero.addEventListener('click', () => {
 		console.log("soy cero")
-		document.querySelector('.valor-inferior').value += 0
+		document.querySelector('.valor-inferior').value += '0'
 	})
 
-	const numeroUno = document.querySelector("#uno")
+	// NUMERO UNO
 	numeroUno.addEventListener('click', () => {
 		console.log("soy uno");
-		document.querySelector('.valor-inferior').value += 1
+		document.querySelector('.valor-inferior').value += '1'
 	})
 
-	const numeroDos = document.querySelector("#dos")
+	// NUMERO DOS
 	numeroDos.addEventListener('click', () => {
 		console.log("soy dos");
-		document.querySelector('.valor-inferior').value += 2
+		document.querySelector('.valor-inferior').value += '2'
 	})
 
-	const numeroTres = document.querySelector("#tres")
+	// NUMERO TRES
 	numeroTres.addEventListener('click', () => {
 		console.log("soy tres")
-		document.querySelector('.valor-inferior').value += 3
+		document.querySelector('.valor-inferior').value += '3'
 	})
 
-	const numeroCuatro = document.querySelector("#cuatro")
+	// NUMERO CUATRO
 	numeroCuatro.addEventListener('click', () => {
 		console.log("soy cuatro")
-		document.querySelector('.valor-inferior').value += 4
+		document.querySelector('.valor-inferior').value += '4'
 	})
 
-	const numeroCinco = document.querySelector("#cinco")
+	// NUMERO CINCO
 	numeroCinco.addEventListener('click', () => {
 		console.log("soy cinco")
-		document.querySelector('.valor-inferior').value += 5
+		document.querySelector('.valor-inferior').value += '5'
 	})
 
-	const numeroSeis = document.querySelector("#seis")
+	// NUMERO SEIS
 	numeroSeis.addEventListener('click', () => {
 		console.log("soy seis")
-		document.querySelector('.valor-inferior').value += 6
+		document.querySelector('.valor-inferior').value += '6'
 	})
 
-	const numeroSiete = document.querySelector("#siete")
+	// NUMERO SIETE
 	numeroSiete.addEventListener('click', () => {
 		console.log("soy siete")
-		document.querySelector('.valor-inferior').value += 7
+		document.querySelector('.valor-inferior').value += '7'
 	})
 
-	const numeroOcho = document.querySelector("#ocho")
+	// NUMERO OCHO	
 	numeroOcho.addEventListener('click', () => {
 		console.log("soy ocho")
-		document.querySelector('.valor-inferior').value += 8
+		document.querySelector('.valor-inferior').value += '8'
 	})
 
-	const numeroNueve = document.querySelector("#nueve")
+	// NUMERO NUEVE
 	numeroNueve.addEventListener('click', () => {
 		console.log("soy nueve")
-		document.querySelector('.valor-inferior').value += 9
+		document.querySelector('.valor-inferior').value += '9'
 	})
 
-	const caracterDecimal = document.querySelector("#punto")
+	// CARACTER DECIMAL
 	caracterDecimal.addEventListener('click', () => {
 		console.log("soy punto")
 		document.querySelector('.valor-inferior').value += '.'
 	})
 
 
-	//* obtencion de operadores
+	//* Obtencion de operadores
 
 	const operadorDivision = document.querySelector("#dividir")
+	const operadorMultiplicar = document.querySelector("#multiplicar")
+	const operadorSuma = document.querySelector("#sumar")
+	const operadorResta = document.querySelector("#restar")
+
+
+	//* Funcionalidad de cada operador
+
+	// DIVISION
 	operadorDivision.addEventListener('click', () => {
 		console.log("soy division")
 		document.querySelector('.valor-inferior').value += ' / '
 	})
 
-	const operadorMultiplicar = document.querySelector("#multiplicar")
+	// MULTIPLICACION
 	operadorMultiplicar.addEventListener('click', () => {
 		console.log("soy multiplicacion")
 		document.querySelector('.valor-inferior').value += ' X '
 	})
 
-	const operadorSuma = document.querySelector("#sumar")
+	// SUMA
 	operadorSuma.addEventListener('click', () => {
 		console.log("soy suma")
 		document.querySelector('.valor-inferior').value += ' + '
 	})
 
-	const operadorResta = document.querySelector("#restar")
+	// RESTA
 	operadorResta.addEventListener('click', () => {
 		console.log("soy resta")
 		document.querySelector('.valor-inferior').value += ' - '
 	})
-
-	//* Funcionalidad del boton C para borrar todo
-	
 
 
 })
